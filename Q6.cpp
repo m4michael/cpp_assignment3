eda591284b99559c131fbb44af079f9586f906f5
Michael Miki Murmu <114741392+m4michael@users.noreply.github.com>
2023-02-22 12:46:09 +0530

    Update Q6.cpp

--- Q6.cpp
+++ Q6.cpp
@@ -12,38 +12,37 @@ if(num>=90 && num<=100){
     cout<<"\nYour grade is A+ ";
 }
 
-else if(num>=80){
+else if(num>=80 && num<90){
     cout<<"\nYour grade is A ";
 }
-else if (num>=70){
+else if (num>=70 & num<80){
     cout<<"\nYour grade is B+";
 }
-else if(num>=60){
+else if(num>=60 && num<70){
     cout<<"\nYour grade is B";
 }
 
-else if (num>=50){
+else if (num>=50 && num<60){
     cout<<"\nYour grade is C ";
 }
 
-else if(num>=40){
+else if(num>=40 && num<50){
     cout<<"\nYour grade is D";
 }
 
-else if(num>=30){
+else if(num>=30 && num<40){
     cout<<"\nYour grade is E";
 }
 
+else if(num<30  && num>0){
+
+    cout<<"\n your grade is F";
+}
+
 else{
-    cout<<"\nYour grade is F ";
+    cout<<"\nEnter marks between 0 to 100";
 }
     
-
-
-
-
-
-
 
     return 0;
 }
